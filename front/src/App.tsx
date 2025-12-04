ad592f38d645d0cdf69da94dddb90cccde066a00
princemabs <princemabenguebebongsujumior@gmail.com>
2025-12-04 22:24:14 +0100

    landing page creation

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import LandingPage from './LandingPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
     <Router>
       <Routes>
          {/* PAGE D’ACCUEIL */}
-        <Route path="/" element={<div>App</div>} />
+        <Route path="/" element={<LandingPage />} />
         {/* SIGNUP */}
         <Route path="/signup" element={<div>Signup</div>} />
         {/* LOGIN */}
