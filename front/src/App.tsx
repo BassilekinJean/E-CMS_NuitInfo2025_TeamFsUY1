2b020b15c95a0554088631ee0e557642d3355ba2
princemabs <princemabenguebebongsujumior@gmail.com>
2025-12-05 00:08:42 +0100

    final version 1.0

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
-
+import { Features } from './components/Features';
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} 
+        children={
+          <Route path="/features" element={<Features />} />
+        }
+        />
       </Routes>
     </Router>
   );
