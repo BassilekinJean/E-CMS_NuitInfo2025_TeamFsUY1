447865621309b574fe027cc344b1d9bb32b6b4f1
princemabs <princemabenguebebongsujumior@gmail.com>
2025-12-04 22:44:37 +0100

    test

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,29 +1,11 @@
-import React from 'react'
-import LandingPage from './LandingPage'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return (
     <Router>
       <Routes>
-         {/* PAGE D’ACCUEIL */}
-        <Route path="/" element={<LandingPage />} />
-        {/* SIGNUP */}
-        <Route path="/signup" element={<div>Signup</div>} />
-        {/* LOGIN */}
-        <Route path="/login" element={<div>Login</div>} />
-        {/* FORGET PASSWORD */}
-        <Route path="/forget-password" element={<div>Forget Password</div>} />
-        {/* RESET PASSWORD */}
-        <Route path="/reset-password" element={<div>Reset Password</div>} />
-        {/* PROFILE */}
-        <Route path="/profile" element={<div>Profile</div>} />
-        {/* SETTINGS */}
-        <Route path="/settings" element={<div>Settings</div>} />
-        {/* LOGOUT */}
-        <Route path="/logout" element={<div>Logout</div>} />
-        {/* NOT FOUND */}
-        <Route path="*" element={<div>404 Not Found</div>} />
-
+        <Route path="/" element={<><p>prince</p></>} />
       </Routes>
     </Router>
   )
---
+++ front/src/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <p>prince</p>
+    </div>
+  )
+}
+
+export default LandingPage
