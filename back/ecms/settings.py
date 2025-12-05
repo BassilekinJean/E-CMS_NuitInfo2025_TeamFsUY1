f58e4f85ee058dd39e0c15ddf93e0fd3353aa913
christotop5 <asangwa.fonayen23@facsciences-uy1.cm>
2025-12-05 07:06:21 +0100

    Add wildcard support for Render domains

--- back/ecms/settings.py
+++ back/ecms/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
+# Change to (development only - not secure for production)
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+if os.environ.get('DEBUG') == 'false':
+    ALLOWED_HOSTS.append('*.onrender.com')
 # Application definition
 
 INSTALLED_APPS = [
